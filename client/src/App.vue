9b80f899e3e7f99c5aaa209c49f220c339096075
Felix Morau <felixmorau@gmail.com>
2018-12-20 10:28:16 +0100

    started working on genres

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/picdic.jpg">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Genre />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Genre from "./components/Genre/Genres.vue"
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Genre
   }
 }
 </script>
---
+++ client/src/components/Genre/Genres.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button v-for="genre in genres" :key="genre.id" @click="chooseGenre(genre.genreName)"> {{genre.genreName}}</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data: function() {
+        return {
+        genres: [
+            {genreName: "Rock", id: 1},
+            {genreName: "Pop", id: 2},
+            {genreName: "Rap", id: 3},
+            {genreName: "Christmas", id: 4}
+            ],
+        genreChosen: null
+        }
+    },
+    methods: {
+        chooseGenre(genre) {
+            this.genreChosen = genre;
+            console.log(genre);
+            this.$emit("genreChoosen", this.genreChosen)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
