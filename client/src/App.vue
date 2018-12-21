0fbc4aba7c5a678b868f52156f82027c9f2a50f1
VictorPersson <vp-96@hotmail.com>
2018-12-21 10:53:04 +0100

    Fixed Felix faulty quotation marks on Imports

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,67 +1,65 @@
 <template>
   <div id="app">
     <div v-if="isGenreChoosen === null">
-      <Genre @genreSelected="genreChoosen" />
+      <Genre @genreSelected="genreChoosen"/>
     </div>
     <div v-else>
-      <p @click="authorizeSpotify"> Genre is chosen </p>
+      <p @click="authorizeSpotify">Genre is chosen</p>
     </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Genre from "./components/Genre/Genres.vue"
+import HelloWorld from "./components/HelloWorld.vue";
+import Genre from "./components/Genre/Genres.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld,
     Genre
   },
   data: function() {
     return {
-    isGenreChoosen: null,
-    playlist: null
-    }
+      isGenreChoosen: null,
+      playlist: null
+    };
   },
   methods: {
     genreChoosen(genreSelected) {
-      this.isGenreChoosen = genreSelected
-      console.log("app state " + this.isGenreChoosen)
+      this.isGenreChoosen = genreSelected;
+      console.log("app state " + this.isGenreChoosen);
     },
     loadPlaylists() {
-      this.$http.get("")
+      this.$http.get("");
     },
     authorizeSpotify() {
-      
-      this.$http.get("https://api.spotify.com/v1/playlists/59ZbFPES4DQwEjBpWHzrtC")
-                      .then(response => {
-                          return response.json()
-                      })
-                      .then(data => {
-                          const resultArray = [];
-                          for (let key in data) {
-                              resultArray.push(data[key])
-                          }
-                          this.playlist = resultArray;
-                          console.log(this.playlist[12].items[1].track.name)
-                      })
+      this.$http
+        .get("https://api.spotify.com/v1/playlists/59ZbFPES4DQwEjBpWHzrtC")
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const resultArray = [];
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+          this.playlist = resultArray;
+          console.log(this.playlist[12].items[1].track.name);
+        });
 
-
-      
-    // this.$http.get("https://api.spotify.com/v1/playlists/59ZbFPES4DQwEjBpWHzrtC")
-    //             .then(response => {
-    //                 console.log(response)
-    //             }, error => {
-    //             console.log(error)}
-    //             )
-    }  
-    },
-    created() {
-      this.authorizeSpotify();
-    } 
+      // this.$http.get("https://api.spotify.com/v1/playlists/59ZbFPES4DQwEjBpWHzrtC")
+      //             .then(response => {
+      //                 console.log(response)
+      //             }, error => {
+      //             console.log(error)}
+      //             )
+    }
+  },
+  created() {
+    this.authorizeSpotify();
   }
+};
 </script>
 
 <style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from "vue-resource"
-const key = require("./apiKeys.js");
+//const key = require("./apiKeys.js");
+
 
 Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
 // BQDTlmwYgms4HV8Rn8UTi2dJR4KiyjNxjpAd-5BRvDIguGgoDkTJCJolzJC2cFIW6mCLbCUdd7MFeXGT_iE
-Vue.http.options.headers = {"Authorization": "Bearer " + key.authToken()}
+Vue.http.options.headers = { "Authorization": "Bearer " + "BQCyb4CE0ayuptCXxrVbK-gspNPWbliBYPQXbGvtXcKtBZGJ89MDbc9qNTuHIijcMSguQuDJNlWW8CjUT9M" }
 
 new Vue({
   render: h => h(App),
