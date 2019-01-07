b512289fcbd7bd388b8e06b0305138d17d23ad24
VictorPersson <vp-96@hotmail.com>
2019-01-07 13:37:51 +0100

    Restructured Game components & implemented guess inputs

--- client/src/components/Game/Game.vue
+++ client/src/components/Game/Game.vue
@@ -1,19 +1,33 @@
 <template>
     <div>
-        <game-info :isGenreChoosen="isGenreChoosen" />
-        <game-input v-if="isGenreChoosen" />
+        <game-info :isGenreChoosen="isGenreChoosen" 
+                    :playlist="playlist" 
+                    :song="song" 
+                    :artist="artist" 
+                    :image="image" 
+                    :lyrics="lyrics"
+        />
+        <game-input v-if="isGenreChoosen" :words="words"/>
     </div>
     
 </template>
 
 <script>
+
+const axios = require("axios")
+
 import GameInfo from "./GameInfo.vue";
 import GameInput from "./GameInput.vue";
 
 export default {
     data: function() {
         return {
-
+            playlist: "",
+            song: "",
+            artist: "",
+            image: "",
+            lyrics: "",
+            words: ""
         }
     },
     components: {
@@ -21,8 +35,42 @@ export default {
         GameInput
     },
     methods: {
-        
+        getSongs() {
+      // Function for retrieving the song, artist, image and lyrics
+      if (this.isGenreChoosen === "Rock") {
+        this.playlist = "http://localhost:9022/rock"
+      } else if (this.isGenreChoosen === "Christmas") {
+        this.playlist = "http://localhost:9022/christmas"
+      } else if (this.isGenreChoosen === "Pop") {
+        this.playlist = "http://localhost:9022/pop"
+      } else if (this.isGenreChoosen === "Rap") {
+        this.playlist = "http://localhost:9022/rap"
+      }
+      axios.get(this.playlist) //get from the chosen playlist
+        .then(response => {
+          const resultArray = [];
+          for (let key in response) { //organizes the json file
+            resultArray.push(response[key]);
+          }
+          this.playlist = resultArray;
+          this.song = this.playlist[0].Song.song //Playlist[0] = Object which stores information about song
+          this.artist = this.playlist[0].Song.artist
+          this.image = this.playlist[0].Song.image
+          this.lyrics = this.playlist[0].Song.lyrics
+          this.words = this.playlist[0].Song.words
+        })
     },
-    props: ["isGenreChoosen"]
+    setSong() {
+      //function to automaticlly update the current songs to the front-end
+      this.getSongs() // Chooses a song as soon as it is clicked
+      setInterval(() => { //Updates every 5 second to see what song is currently selected
+        this.getSongs()
+      }, 15*1000)
+    },
+    },
+    props: ["isGenreChoosen"],
+    created() {
+      this.setSong()
+    }
 }
 </script>
--- client/src/components/Game/GameInput.vue
+++ client/src/components/Game/GameInput.vue
@@ -1,17 +1,36 @@
 <template>
     <div>
-        <p>hej</p>
+      <input type="text" v-model="userGuess">
+      <button @click="checkWord">Check</button>
     </div>
 </template>
 
 <script>
+const axios = require("axios")
+
 export default {
     data: function() {
         return {
-
+            userGuess: null,
+            score: 0,
+            // Numb of avalible chances to guess (3)?,
         }
     },
-    props: ["isGenreChoosen"]
+
+    methods: {
+        checkWord() {
+            for (var i = 0; i < this.words.length; i++) {
+                if (this.userGuess === this.words[i]) {
+                    this.score++
+                    console.log("Guess is correct ∏")
+                } else {
+                    console.log("Guess is incorrect ∏")
+                }
+            }
+        }
+    },
+
+    props: ["isGenreChoosen", "words"]
 }
 </script>
 
