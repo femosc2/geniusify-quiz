739ec217fac7be4e31e0b3475b600c60adfbc684
Adam Nilsson <AdamNilsson_@hotmail.com>
2019-01-07 11:15:13 +0100

    Placeholder added to NameSubmit button in ChooseName

--- client/src/components/ChooseName/ChooseName.vue
+++ client/src/components/ChooseName/ChooseName.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Welcome to Geniusify-Quiz</h1>
-        <input type="text" v-model="name">
+        <input type="text" v-model="name" placeholder="What's your name?" required>
         <button @click="submit">SUBMIT</button>
     </div>
 </template>
