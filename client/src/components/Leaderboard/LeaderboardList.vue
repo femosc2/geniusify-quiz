b5fa6ba350b125f23d722cbc3039e16bce726cff
Felix Morau <felixmorau@gmail.com>
2019-01-08 19:38:39 +0100

    you can now scroll in the leaderboard

--- client/src/components/Leaderboard/LeaderboardList.vue
+++ client/src/components/Leaderboard/LeaderboardList.vue
@@ -53,7 +53,8 @@ div {
   -webkit-box-shadow: 0px 0px 30px -2px rgba(0, 0, 0, 0.5);
   -moz-box-shadow: 0px 0px 30px -2px rgba(0, 0, 0, 0.5);
   box-shadow: 0px 0px 30px -2px rgba(0, 0, 0, 0.5);
-  overflow: visible;
+  overflow-Y: scroll;
+  height: 100vh;
 }
 .fade-enter-active,
 .fade-leave-active {
@@ -70,7 +71,6 @@ ul {
     list-style-type: none;
     text-align: center;
     padding: 0;
-    overflow: scroll;
 }
 h4 {
     text-align: center;
