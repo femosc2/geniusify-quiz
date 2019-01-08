818bf5a16e40611df4c7520c1f19f8220c81289d
Felix Morau <felixmorau@gmail.com>
2019-01-08 11:28:14 +0100

    made progressbars

--- client/src/components/Game/GameInput.vue
+++ client/src/components/Game/GameInput.vue
@@ -3,9 +3,12 @@
       <input type="text" v-model="userGuess">
       <button @click="checkWord">Check</button>
       <p> {{ score }} </p>
+      <div class="progressBar" />
     </div>
     <div v-else>
-      GAME OVER!
+      <transition>
+      <h1>GAME OVER!</h1>
+      </transition>
     </div>
 </template>
 
@@ -53,7 +56,7 @@ export default {
         setTimeout(function() {
            location.reload();
         }, 2 * 1000)
-      }, 10 * 1000);
+      }, 150 * 1000);
     }
   },
   props: ["isGenreChoosen", "words", "name"],
@@ -64,4 +67,24 @@ export default {
 </script>
 
 <style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+  }
+  .progressBar {
+    height: 100px;
+    background-color: black;
+    width: 0%;
+    animation: progressBar 15s infinite linear;
+  }
+  @keyframes progressBar {
+    0% {
+      width: 0%;
+    }
+    100% {
+      width: 100%;
+    }
+  }
 </style>
---
+++ client/src/components/Game/GameProgress.vue
@@ -0,0 +1,26 @@
+<template>
+    <div />
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+div {
+    height: 100px;
+    background-color: black;
+    width: 0%;
+    animation: progressBar 150s infinite linear;
+  }
+  @keyframes progressBar {
+    0% {
+      width: 0%;
+    }
+    100% {
+      width: 100%;
+    }
+  }
+</style>
